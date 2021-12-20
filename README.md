1a5f22d520dfce283f54d587f3b5a71837db54fb
liuda <liudmyla.iovenko@gmail.com>
2021-12-19 20:19:00 -0500

    updated logic

--- README.md
+++ README.md
@@ -1,8 +1,32 @@
-## Space Battle (OOP console game)
-### 🚀 SPECIFICATIONS
+## Space Battle (JS game)
+### Overview
+Earth has been attacked by a horde of aliens! You are the captain of the USS Schwarzenegger, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers. 
 
-Build a game of battling alien spaceships using Javascript. Earth has been attacked by a horde of aliens! You are the captain of the USS Schwarzenegger, on a mission to destroy every last alien ship. Battle the aliens as you try to destroy them with your lasers. There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.
+### Live Preview
+https://liudmylai.github.io/space_battle/
 
-A game round would look like this: 
+### 🚀 Specifications
+Build a game of battling alien spaceships using Javascript. There are six alien ships. The aliens' weakness is that they are too logical and attack one at a time: they will wait to see the outcome of a battle before deploying another alien ship. Your strength is that you have the initiative and get to attack first. However, you do not have targeting lasers and can only attack the aliens in order. After you have destroyed a ship, you have the option to make a hasty retreat.
 
-You attack the first alien ship - If the ship survives, it attacks you - If you survive, you attack the ship again - If it survives, it attacks you again - Etc. - If you destroy the ship, you have the option to **attack** the next ship or to **retreat** - If you retreat, the game is over, perhaps leaving the game open for further developments or options. - You win the game if you destroy all of the aliens. - You lose the game if you are destroyed.
+A game round looks like this: 
+
+You attack the first alien ship - If the ship survives, it attacks you - If you survive, you attack the ship again - If it survives, it attacks you again - Etc. - If you destroy the ship, you have the option to **attack** the next ship or to **retreat** - If you retreat, the game is over. - You win the game if you destroy all of the aliens. - You lose the game if you are destroyed.
+
+Ship Properties:
+
+**hull** is the same as health. If hull reaches `0` or less, the ship is destroyed.
+**firepower** is the amount of damage done to the **hull** of the target with a successful hit.
+**accuracy** is the chance between 0 and 1 that the ship will hit its target.
+
+### 🚀 Bonuses 
+- [ ] The aliens send a random number of ships to attack Earth. Think of a reasonable range and implement it. 
+- [ ] Scientists have developed a super targeting computer for your lasers. You now are asked which of the aliens you would like to hit with your lasers. 
+- [ ] Scientists have improved your ship's shields. They don't work that consistently, and only improve your hit points by a random number each time 
+- [ ] Scientists have put missiles on your ship. You only have a limited number of them, but they do a lot of damage. You can say before each battle if you want to use one of your missles. 
+- [ ] The aliens have gained emotions and now can attack more than one at a time. 
+- [ ] Evil alien scientists have created an alien mega-ship. This mega-ship contains a number of "weapon pods" that each have their own individual hit points. These "weapon-pods" ( objects ) must all be destroyed before you can begin doing damage to the main ship, which also has its own hit points.
+- [ ] When the game is over, prompt the user if they would like to play again, and make it so the user can play again with all the values set back to default. 
+- [ ] So far the entire game is just one battle, with many aliens. implement a game that consists of multiple battles where enemies respawn for a new battle at the end of the old battle. Keep track of points for the number of wins the player has. 
+- [ ] After every battle you are asked if you want to return to base and recharge your shields. 
+- [ ] Make the players and enemies stronger after each battle 
+- [ ] Distribute medals and power ups to the player depending on points
--- app.js
+++ app.js
@@ -1,88 +1,144 @@
-/**
- * Superclass
- */
+// Superclass
 class SpaceShip {
+
+    constructor(name) {
+        this.name = name;
+    }
+    /**
+     * Attack the enemy
+     * @param {obj} enemy
+     * @return {number} enemy hull
+     */
     attack(enemy) {
         if (Math.random() < this.accuracy) {
             enemy.hull -= this.firepower;
-            log(`${this.name} hit ${enemy.name} on ${this.firepower}`)
+            enemy.showStats();
+            log(`${this.name} hit ${enemy.name} [${this.firepower}]`)
         } else {
             log(`${this.name} missed`)
         }
         return enemy.hull;
     }
+    showStats() {
+        document.getElementById(this.statsId).innerHTML = `Hull : ${this.hull}<br>FirePower : ${this.firepower}<br>Accuracy : ${this.accuracy}`;
+    }
 }
-/**
- * The USS Schwarzenegger should have the following properties:
- *  - hull      : 20
- *  - firepower : 5
- *  - accuracy  : 0.7
- */
+
+// USS Space Ship class
 class USS extends SpaceShip {
+    /**
+     * The USS Schwarzenegger has the following properties:
+     *  - hull      : 20
+     *  - firepower : 5
+     *  - accuracy  : 0.7
+     * @param {string} name // spaces ship name
+     * @return {USS} // USS space ship
+     */
     constructor(name) {
-        super();
-        this.name = name;
+        super(name);
         this.hull = 20;
         this.firepower = 5;
         this.accuracy = 0.7;
+        this.statsId = 'playerStats';
+    }
+    /**
+     * Attack the enemy
+     * @param {string} message // prompt message
+     * @param {string[]} options // prompt options
+     * @return {string} // user input
+     */
+    prompt(message, options) {
+        let input;
+        do {
+            input = prompt(message, options.join('/'));
+            if (input === null) {
+                return null;
+            } else if (options.includes(input.toLowerCase())) {
+                return input.toLowerCase();
+            }
+        } while (input !== null)
     }
 }
-/**
- * The alien ships should each have the following ranged properties determined randomly: 
- *  - hull      : between 3 and 6
- *  - firepower : between 2 and 4 
- *  - accuracy  : between 0.6 and 0.8
- */
+
+// Alien Space Ship class
 class Alien extends SpaceShip {
+    /**
+     * The alien ship has the following ranged properties determined randomly: 
+     *  - hull      : between 3 and 6
+     *  - firepower : between 2 and 4 
+     *  - accuracy  : between 0.6 and 0.8
+     * @param {string} name // spaces ship name
+     * @return {Alien} // Alien space ship
+     */
     constructor(name) {
-        super();
-        this.name = name;
+        super(name);
         this.hull = 3 + Math.round((6 - 3) * Math.random());
         this.firepower = 2 + Math.round((4 - 2) * Math.random());
         this.accuracy = 0.6 + Math.round((0.8 - 0.6) * Math.random() * 10) / 10;
+        this.statsId = 'enemyStats';
+    }
+    /**
+     * @param {number} numAlienShips
+     * @return {Alien[]} Array of 'Alien' objects
+     */
+    static createArmada(numAlienShips) {
+        const armada = [];
+        for (let i = 0; i < numAlienShips; i++) {
+            armada.push(new Alien(`Alien#${i + 1}`))
+        }
+        return armada;
     }
 }
 
 /**
  * @param {USS} hero
- * @param {number} enemies
- * @return  1: Hero won
- *          0: Hero lose
- *         -1: Hero retreated
+ * @param {Alien[]} enemies
+ * @return {-1|0|1} 1 - Hero won, 0 - Hero lose, -1 - Hero retreated
  */
 const battle = (hero, enemies) => {
-    while (enemies > 0) {
-        const target = new Alien('Alien');
-        while (target.hull > 0) {
 
-            log(`[Current Health: ${hero.hull}] [Target's Health: ${target.hull}] [Enemies Remaining:${enemies}]`)
+    let round = 0;
+    // continue battle while number of enemies more than zero
+    while (enemies.length > 0) {
 
-            // hero attacs
-            if (hero.attack(target) <= 0) {
-                enemies--;
-                log(`${target.name} is destroyed!`)
-            } else if (target.attack(hero) <= 0) {
-                //game over
-                log(`Game over`);
-                return 0;
+        const target = enemies.pop();
+        target.showStats();
+        let heroAction = hero.prompt('[Current Health: ' + hero.hull + '] [Target\'s Health: ' + target.hull + '] [Enemies Remaining: ' + (enemies.length + 1) + ']\n\nDo you want to attack the first alien ship?', ['attack', 'retreat']);
+
+        if (heroAction === 'retreat' || heroAction === null) {
+
+            log(`${hero.name} retreat! ===> :::[ GAME OVER ]:::`);
+            return -1;
+
+        } else {
+
+            while (target.hull > 0) {
+                log(`:::[ ROUND ${++round} ]:::`);
+
+                // hero attacs
+                if (hero.attack(target) <= 0) {
+                    log(`${target.name} is DESTROYED!`)
+                } else if (target.attack(hero) <= 0) {
+                    //game over
+                    log(`:::[ GAME OVER ]:::`);
+                    return 0;
+                }
+
             }
-
-            // next round
-            log(`next round`)
         }
-        // next round
-        log(`next target`)
-
-    }
-    return 1;
 }
-
+log(`:::[ ${hero.name} WON ]:::`);
+return 1;
+}
+// function to log into console
 const log = str => console.log(str);
 
-const schwarzenegger = new USS('USS Schwarzenegger');
-
-let enemies = 6;
-
-battle(schwarzenegger, enemies);
+window.addEventListener('load', function () {
+    // Start Program
+    const schwarzenegger = new USS('USS Schwarzenegger');
+    schwarzenegger.showStats();
+    const enemies = Alien.createArmada(6);
+    battle(schwarzenegger, enemies);
+});
 
 
